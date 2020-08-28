e93afe4ec7e82db045c4922db4bbb84cbc73bbf6
Volkov Alexander <volkov.525@gmail.com>
2020-08-28 15:02:07 +0300

    add buttons block

---
+++ src/components/AppButtons.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+.action
+	v-btn(depressed :color="color" dark :block="block" ) На согласование
+	v-btn(depressed color="blue-grey lighten-4" :block="block") На доработку
+	v-btn(text :block="block" v-show="$vuetify.breakpoint.mdAndUp") Ещё
+	v-btn(depressed color="blue-grey lighten-4" v-show="$vuetify.breakpoint.sm") В архив
+	v-btn(outlined color="pink" v-show="$vuetify.breakpoint.sm") Удалить
+
+</template>
+
+<script>
+
+export default {
+	props: ['color'],
+	data () {
+		return {
+		}
+	},
+	computed: {
+		block () {
+			if (this.$vuetify.breakpoint.sm) {
+				return false
+			} else return true
+		},
+	},
+
+}
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/css/colors.scss';
+.action {
+	.v-btn {
+		margin-bottom: .25rem;
+		margin-right: .25rem;
+	}
+}
+
+</style>
--- src/components/Doc1.vue
+++ src/components/Doc1.vue
@@ -1,16 +1,26 @@
 <template lang="pug">
 .doc
-	div.zg
+	.fix.zg
 		Zag
-	div.but  кнопки действий
-	div.main  основная.область
-	div.attr атрибуты
+	.fix.but
+		AppButtons(:color="maincolor")
+	.fix.main  
+		p основная.область
+		p основная.область
+		p основная.область
+		p основная.область
+		p основная.область
+	.fix.attr
+		p атрибуты
+		p атрибуты
 
 
 </template>
 
 <script>
 import Zag from '@/components/Zag'
+import AppButtons from '@/components/AppButtons'
+
 
 export default {
 	props: {
@@ -21,6 +31,7 @@ export default {
 	},
 	components: {
 		Zag,
+		AppButtons,
 	},
 
 	data () {
@@ -37,44 +48,43 @@ export default {
 .doc {
 	margin-bottom: 5rem;
 	display: grid;
-	gap: 1rem;
+	gap: 2rem;
 	grid-template-columns: repeat(8, 1fr);
-	grid-template-rows: repeat (min-content, auto);
+	/* @media (max-width: 960px) { */
+	/* 	gap: 1rem; */
+	/* } */
 	> div {
 		background: #ccc;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 	}
-	div:first-child {
+	.fix {
 		background: none;
 		display: block;
 	}
 	.zg {
 		grid-column: span 6;
-		@media (max-width: 1200px) {
+		@media (max-width: 1264px) {
 			grid-column: span 8;
 		}
 	}
 	.but {
 		grid-column: span 2;
-		grid-row: span 2;
-		height: 150px;
-		@media (max-width: 1200px) {
+		@media (max-width: 1264px) {
 			grid-column: 7/9;
 			grid-row: 2/3;
 		}
-		@media (max-width: 900px) {
+		@media (max-width: 960px) {
 			grid-column: 1/9;
 			grid-row: 2/3;
-			height: 50px;
 		}
 	}
 	.main {
 		grid-column: span 6;
 		grid-row: span 2;
-		height: 300px;
-		@media (max-width: 900px) {
+		/* height: 300px; */
+		@media (max-width: 960px) {
 			grid-column: 1/6;
 		}
 		@media (max-width: 800px) {
@@ -83,8 +93,8 @@ export default {
 	}
 	.attr {
 		grid-column: 7/9;
-		height: 200px;
-		@media (max-width: 900px) {
+		/* height: 200px; */
+		@media (max-width: 960px) {
 			grid-column: 6/9;
 		}
 		@media (max-width: 800px) {
