7565ad784efb84b0e4a5940f20da7a2f62213e82
Volkov Alexander <volkov.525@gmail.com>
2020-09-03 10:31:18 +0300

    continue with attributes

--- src/components/Attributes.vue
+++ src/components/Attributes.vue
@@ -7,17 +7,27 @@
 			v-expansion-panel-content
 				.section(v-if="grid")
 					template(v-for="item in attributes")
-						.label(:class="item.clas") {{ item.label }}:
-						.value(v-if="item.id === 13")
-							v-icon(small v-if="item.icon") mdi-calendar
-							|{{ item.value[0] }}<br>{{ item.value[1] }}<br>{{ item.value[2] }}
-						.value(v-else)
-							v-icon(small v-if="item.icon") mdi-calendar
-							|{{ item.value }}
 
-						.check(v-if="item.id === 12")
-							|<span>&#9886;</span> По договорной деятельности<br>
-							|<span>&#9886;</span> Конфиденциально<br>
+						template(v-if="item.id === 2 || item.id === 14 || item.id === 18")
+							.label(:class="item.clas") {{ item.label }}:
+							v-menu(v-model="item.date" :close-on-content-click="true" :nudge-right="40" transition="scale-transition" offset-y min-width="290px").value
+								template(v-slot:activator="{ on, attrs }")
+									.value(v-bind="attrs" v-on="on")
+										v-icon(small) mdi-calendar
+										|{{ item.value }}
+								v-date-picker
+
+						template(v-if="item.id === 13")
+							.label(:class="item.clas") {{ item.label }}:
+							.value {{ item.value[0] }}<br>{{ item.value[1] }}<br>{{ item.value[2] }}
+
+						template(v-if="item.id !== 2 && item.id !== 13 && item.id !== 14 && item.id !== 18")
+							.label(:class="item.clas") {{ item.label }}:
+							.value(:contenteditable="item.edit") {{ item.value }}
+							.check(v-if="item.id === 12")
+								|<span>&#9886;</span> По договорной деятельности<br>
+								|<span>&#9886;</span> Конфиденциально<br>
+
 				.new(v-else v-for="item in attributes")
 					.label(:class="item.clas") {{ item.label }}:
 					.value(v-if="item.id === 13")
@@ -31,10 +41,6 @@
 							|<span>&#9886;</span> Конфиденциально<br>
 				v-btn(text x-small @click.stop="grid = !grid" color="#aaa").mt-5 switch
 
-				v-menu(v-model="menu2" :close-on-content-click="true" :nudge-right="40" transition="scale-transition" offset-y min-width="290px")
-					template(v-slot:activator="{ on, attrs }")
-						.value(v-bind="attrs" v-on="on") {{date}}
-					v-date-picker
 </template>
 
 <script>
@@ -42,29 +48,28 @@
 export default {
 	data: () => ({
 		one: 0,
+		items: ['one', 'two', 'three'],
 		grid: true,
-		menu2: false,
-		date: '03.09.2020',
 		attributes: [
 			{ id:  0, label: 'Вид СЗ', value: 'СЗ на предоставление доступа' },
-			{ id:  1, label: 'Рег. №', value: '16-8' },
-			{ id:  2, label: 'Дата рег.', value: '16.06.20', icon: true },
+			{ id:  1, label: 'Рег. №', value: '16-8', edit: true },
+			{ id:  2, label: 'Дата рег.', value: '16.06.20', icon: true, date: false },
 			{ id:  3, label: 'Автор докум.', value: 'Макарова Е.В.' },
-			{ id:  4, label: 'Тел. автора', value: '8 904 230-45-32' },
+			{ id:  4, label: 'Тел. автора', value: '8 904 230-45-32', edit: true },
 			{ id:  5, label: 'Подразд.', value: '16 ОАП - Отдел автомазации проектированияи инф.технологий' },
-			{ id:  6, label: 'Тема', value: 'О создании рабочей группы' },
+			{ id:  6, label: 'Тема', value: 'О создании рабочей группы', edit: true  },
 			{ id:  7, label: 'Подписывает', value: 'Лебедской-Тамбиев М.А.' },
 			{ id:  8, label: 'ГИП', value: 'dv3_ГИП, Юзер3' },
-			{ id:  9, label: 'Примечание', value: 'Макет карточки макет карточки макет карточки' },
-			{ id: 10, label: 'Проект', value: 'Проект А' },
+			{ id:  9, label: 'Примечание', value: 'Макет карточки макет карточки макет карточки', edit: true  },
+			{ id: 10, label: 'Проект', value: 'Проект А', edit: true  },
 			{ id: 11, label: 'Приоритет', value: 'средний' },
 			{ id: 12, label: 'Тип документации', value: 'Нестандартизированное оборудование' },
 			{ id: 13, label: 'Исполнители', value: ['Агаева И.', 'Петрова С.', 'Булочкина К.']},
-			{ id: 14, label: 'Дата испол.', value: '18.06.20', icon: true },
+			{ id: 14, label: 'Дата испол.', value: '18.06.20', icon: true, date: false },
 			{ id: 15, label: 'Копия', value: 'Павленикова А.Г.', },
 			{ id: 16, label: 'В отделы', value: 'ОАП, ТО' },
 			{ id: 17, label: 'Предв. №', value: '96-2020' },
-			{ id: 18, label: 'Дата созд.', value: '16.06.2020 19:25' },
+			{ id: 18, label: 'Дата созд.', value: '16.06.2020 19:25', icon: true, date: false },
 			{ id: 19, label: 'Создал', value: 'Сергиенко К.' },
 		],
 	}),
@@ -102,6 +107,12 @@ export default {
 		&:hover {
 			border-bottom: 1px dotted $link;
 		}
+		&:focus {
+			border: none;
+			outline: 1px dotted $link;
+			padding: 3px;
+			color: black;
+		}
 	}
 }
 .check {
@@ -133,6 +144,11 @@ export default {
 	.check {
 		flex: 1 1 100%;
 	}
+}
+.test {
+	width: 100%;
+	height: 50px;
+	background: red;
 }
 </style>
 
